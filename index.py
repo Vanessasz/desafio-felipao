a88b7066a722d1b630c49228470f1830f1e39216
Vanessa S Helena <vanessa182vans@gmail.com>
2024-04-29 16:52:55 -0300

    Desafio classificador de nível de herói concluído

--- index.py
+++ index.py
@@ -1,1 +1,25 @@
-print("Olá mundo")
+#Desafio Classificador de nível de Herói
+
+nomeDoHeroi = input("Qual é o nome do Herói? ")
+quantdXpHeroi = int (input("Que nível está o Herói? "))
+
+if quantdXpHeroi <= 1000:
+    print("O Herói" ,nomeDoHeroi, "está no nível de: Ferro")
+elif quantdXpHeroi >= 1001 and quantdXpHeroi <= 2000: 
+    print("O Herói" ,nomeDoHeroi, "está no nível de: Bronze")
+elif quantdXpHeroi >= 2001 and quantdXpHeroi <= 5000:
+    print("O Herói" ,nomeDoHeroi, "está no nível de: Prata")
+elif quantdXpHeroi >= 5001 and quantdXpHeroi <= 7000:
+    print("O Herói" ,nomeDoHeroi, "está no nível de: Ouro")
+elif quantdXpHeroi >= 7001 and quantdXpHeroi <= 8000:
+    print("O Herói" ,nomeDoHeroi, "está no nível de: Platina")
+elif quantdXpHeroi >=  8001 and quantdXpHeroi <= 9000:
+    print("O Herói" ,nomeDoHeroi, "está no nível de: Ascendente")
+elif quantdXpHeroi >= 9001 and quantdXpHeroi <= 10000:
+    print("O Herói" ,nomeDoHeroi, "está no nível de: Imortal")
+else: 
+    print("Seu Herói está no nível de: Radiante")
+
+
+
+
